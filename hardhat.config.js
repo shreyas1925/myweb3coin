8e0150b8e5881b423079f6e1c3c93916c96bd8dd
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-06-01 10:06:08 +0530

    initial refractor

---
+++ hardhat.config.js
@@ -0,0 +1,31 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const privateKey = "8a72f8d3a4493ef5df08dc1f68b12284d415e79afccda62c309e7e3928f26f84"
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
+      accounts: [privateKey]
+    }
+  },
+};
